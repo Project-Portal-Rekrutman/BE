b86755de1659df84632fd6df23f4d240ab4793d2
Nasri <nasrisarif98@gmail.com>
2024-03-28 13:18:17 +0700

    first commit

---
+++ src/main/java/com/example/Portal_Recruitment/PortalRecruitmentApplication.java
@@ -0,0 +1,13 @@
+package com.example.Portal_Recruitment;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PortalRecruitmentApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PortalRecruitmentApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/Portal_Recruitment/PortalRecruitmentApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.Portal_Recruitment;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PortalRecruitmentApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
