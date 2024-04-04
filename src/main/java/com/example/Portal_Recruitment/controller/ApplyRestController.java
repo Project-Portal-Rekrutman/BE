6b1cd88570702efe8ce72e3b14c6eedd0451ee12
Luqmanhr25 <158405379+Luqmanhr25@users.noreply.github.com>
2024-04-04 08:07:18 +0700

    Delete import

--- src/main/java/com/example/Portal_Recruitment/controller/ApplyRestController.java
+++ src/main/java/com/example/Portal_Recruitment/controller/ApplyRestController.java
@@ -1,12 +1,12 @@
 package com.example.Portal_Recruitment.controller;
 
-import java.util.Date;
 import java.time.LocalDate;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -70,6 +70,11 @@ public class ApplyRestController {
     
         applyRepository.save(apply);
         return CustomResponse.generate(HttpStatus.OK, "Data Successfully Added");
+    }
+
+    @GetMapping("apply/{id}")
+    public ResponseEntity<Object> getProgressList(@PathVariable Integer id) {
+        return CustomResponse.generate(HttpStatus.OK, "Data Successfully Fetched", applyRepository.getProgress(id));
     }
 }
     
--- src/main/java/com/example/Portal_Recruitment/repository/ApplyRepository.java
+++ src/main/java/com/example/Portal_Recruitment/repository/ApplyRepository.java
@@ -1,5 +1,7 @@
 package com.example.Portal_Recruitment.repository;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 
@@ -13,5 +15,20 @@ public interface ApplyRepository extends JpaRepository<Apply, Integer>{
 
     @Query(value = "Select * from tb_tr_apply where id= ?1", nativeQuery = true)
     public Apply getIdApply(Integer Id);
+
+    @Query(value = 
+    "SELECT 'Send Application' AS Progress, ap.application_status AS Status, ap.application_date AS Date " +
+    "FROM tb_tr_apply ap " +
+    "WHERE ap.id = ?1 " +
+    "UNION ALL " +
+    "SELECT 'Screening CV' AS Progress, ap.screening_status AS Status, ap.screening_date AS Date " +
+    "FROM tb_tr_apply ap " +
+    "WHERE ap.id = ?1 " +
+    "UNION ALL " +
+    "SELECT 'Interview' AS Progress, te.interview_status AS Status, te.interview_status_date AS Date " +
+    "FROM tb_tr_apply ap " +
+    "LEFT JOIN tb_tr_interview te ON te.apply_id = ap.id " +
+    "WHERE ap.id = ?1", nativeQuery = true)
+public List<Object[]> getProgress(Integer Id);
 }
 
